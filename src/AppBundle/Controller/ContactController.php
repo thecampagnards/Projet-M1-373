c97c2cff45205487a5d0bff9ff015acfa0d674a0
TheCampagnards <konstantin.sidorenko@orange.fr>
2017-02-17 15:31:24 +0100

    [CONTACT] - ajout du formulaire de contact

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Form\ContactType;
+use AppBundle\Entity\Contact;
+
 class ContactController extends Controller
 {
     /**
@@ -13,6 +16,39 @@ class ContactController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('pages/contact.html.twig');
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+          if($form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+            //creation du mail
+            $adminMails = $this
+                    ->getDoctrine()
+                    ->getManager()
+                    ->getRepository('AppBundle:Utilisateur')->findEmailsByRole('ADMIN');
+
+            $message = \Swift_Message::newInstance()
+              ->setSubject('Contact depuis votre site')
+              ->setFrom($this->getParameter('mailer_user'))
+              ->setTo($adminMails)
+              ->setBody(
+                $this->renderView(
+                  'emails/contact.html.twig',
+                    array('contact' => $contact)
+                  ),
+                'text/html'
+              );
+            //envoi du mail
+            $this->get('mailer')->send($message);
+            return $this->render('pages/contact.html.twig', array('form' => $form->createView(), 'valide' => true));
+          }else{
+            return $this->render('pages/contact.html.twig', array('form' => $form->createView(), 'valide' => false));
+          }
+        }
+
+        return $this->render('pages/contact.html.twig', array('form' => $form->createView()));
     }
 }
---
+++ src/AppBundle/Entity/Contact.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contact
+ *
+ * @ORM\Table(name="contact")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255, nullable=true)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="text", nullable=true)
+     */
+    private $message;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    public function __toString()
+    {
+        return (string) $this->nom;
+    }
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->date = new \DateTime("now");
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Contact
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Contact
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Contact
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
