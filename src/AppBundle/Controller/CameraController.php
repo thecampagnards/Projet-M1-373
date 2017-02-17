f2c682eee078de5e6b8ec5dd3e74fdddf8478464
Evan Guélard <evan.guelard@gmail.com>
2017-02-17 15:16:12 +0100

    [FRONT] - vue / ctrl

--- src/AppBundle/Controller/CameraController.php
+++ src/AppBundle/Controller/CameraController.php
@@ -2,17 +2,28 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Camera;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class CameraController extends Controller
 {
     /**
      * @Route("/cameras", name="cameras")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        return $this->render('pages/cameras.html.twig');
+        $cameras = $this->getDoctrine()->getRepository('AppBundle:Camera')->findAll();
+        return $this->render('pages/cameras.html.twig', array('cameras' => $cameras));
+    }
+
+    /**
+ * @Route("/camera/{id}", name="camera")
+ * @ParamConverter("camera", class="AppBundle:Camera")
+ */
+    public function cameraAction(Camera $camera)
+    {
+        return $this->render('pages/camera.html.twig', array('camera' => $camera));
     }
 }
--- src/AppBundle/Controller/MediaController.php
+++ src/AppBundle/Controller/MediaController.php
@@ -2,16 +2,17 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Media;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class MediaController extends Controller
 {
     /**
      * @Route("/medias", name="medias")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         return $this->render('pages/medias.html.twig');
     }
@@ -19,16 +20,27 @@ class MediaController extends Controller
     /**
      * @Route("/populaires", name="medias_populaires")
      */
-    public function populairesAction(Request $request)
+    public function populairesAction()
     {
-        return $this->render('pages/populaires.html.twig');
+        $medias = $this->getDoctrine()->getRepository('AppBundle:Media')->findBy(array(), array('vote' => 'DESC'));
+        return $this->render('pages/medias.html.twig', array('medias' => $medias));
     }
 
     /**
      * @Route("/derniers", name="medias_derniers")
      */
-    public function derniersAction(Request $request)
+    public function derniersAction()
     {
-        return $this->render('pages/derniers.html.twig');
+        $medias = $this->getDoctrine()->getRepository('AppBundle:Media')->findBy(array(), array('vote' => 'DESC')); //date
+        return $this->render('pages/medias.html.twig', array('medias' => $medias));
+    }
+
+    /**
+     * @Route("/medias/{id}", name="media")
+     * @ParamConverter("media", class="AppBundle:Media")
+     */
+    public function mediaAction(Media $media)
+    {
+        return $this->render('pages/media.html.twig', array('media' => $media));
     }
 }
